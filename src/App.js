f1a6903d5ae1de1fcdf573cecd927dff8c10b2d2
SANDIP SADHUKHAN <sandippakhanna@gmail.com>
2021-03-05 07:28:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [color1, setColor1] = useState('#8E2DE2');
+  const [color2, setColor2] = useState('#4A00E0');
+
+  const handleChangeComplete1 = (color) => {
+    setColor1(color)
+  };
+  const handleChangeComplete2 = (color) => {
+    setColor2(color)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper" style={{ backgroundImage: `linear-gradient(to right,${color1},${color2}` }}>
+      < h1 className="title" > Linear Gradient</h1 >
+      <h3 className="subtitle">Pick Your colors</h3>
+      <div className="colors">
+        <input type="color" value={color1} onChange={e => handleChangeComplete1(e.target.value)} />
+        <input type="color" value={color2} onChange={e => handleChangeComplete2(e.target.value)} />
+      </div>
+      <h3 className="subtitle">CSS Code</h3>
+      <p className="code">background-image : linear-gradient(to right,{color1},{color2});</p>
+      <button className="btn" onClick={() => navigator.clipboard.writeText(`background-image : linear-gradient(to right,${color1},${color2})`)} >Click to Copy</button>
+    </div >
   );
 }
 
